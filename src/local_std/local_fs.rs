c4d9f0a5f13dd5542ee57bfdf32f1fb6218c79bd
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-14 14:44:51 +0300

    chore: add additional test to read the file content

--- src/local_std/local_fs.rs
+++ src/local_std/local_fs.rs
@@ -5,17 +5,18 @@ pub fn execute_create_dir(path: &Path) -> std::io::Result<()> {
     Ok(())
 }
 
-
 pub fn execute_delete_dir(path: &Path) -> std::io::Result<()> {
     fs::remove_dir(path)?;
     Ok(())
 }
 
-pub fn execute_create_file<T: AsRef<Path>, C: AsRef<[u8]>>(path: T, content: C) -> std::io::Result<()> {
+pub fn execute_create_file<T: AsRef<Path>, C: AsRef<[u8]>>(
+    path: T,
+    content: C,
+) -> std::io::Result<()> {
     fs::write(path, content)?;
     Ok(())
 }
-
 
 #[cfg(test)]
 mod test {
@@ -42,5 +43,14 @@ mod test {
         let path = Path::new("./test.txt");
         execute_create_file(path, "Hello world").expect("");
         assert!(fs::exists(path).expect("Cannot check if file is there"));
+    }
+
+    #[test]
+    fn should_read_file_content() {
+        let path = Path::new("./test.txt");
+        let expected_file_content: String = "Hello world".to_string();
+        let actual_file_content: String =
+            fs::read_to_string(path).expect("Cannot read file content");
+        assert_eq!(expected_file_content, actual_file_content);
     }
 }
