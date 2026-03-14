2098e3f8f5e7c6199795d050833c80625b0a6869
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-14 14:59:20 +0300

    fix: test were asynchrone

--- src/local_std/local_fs.rs
+++ src/local_std/local_fs.rs
@@ -26,15 +26,17 @@ mod test {
 
     #[test]
     fn should_create_single_dir() {
-        let path = Path::new("./test-folder");
+        let path = Path::new("./first-dir-test");
         execute_create_dir(path).expect("Cannot create test-output-folder");
         assert!(fs::exists(path).expect("Cannot check folder existance"));
+        fs::remove_dir(path).expect("Cannot delete dir");
     }
 
     #[test]
     fn should_delete_single_folder() {
-        let path = Path::new("./test-folder");
-        execute_delete_dir(path).expect("Cannot delete test-output-folder");
+        let path = Path::new("./second-dir-test");
+        fs::create_dir(path).unwrap();
+        fs::remove_dir(path).expect("Cannot delete second-test-dir");
         assert!(!fs::exists(path).expect("Cannot check if folder is still there"));
     }
 
@@ -43,14 +45,25 @@ mod test {
         let path = Path::new("./test.txt");
         execute_create_file(path, "Hello world").expect("");
         assert!(fs::exists(path).expect("Cannot check if file is there"));
+        fs::remove_file(path).unwrap();
     }
 
     #[test]
     fn should_read_file_content() {
         let path = Path::new("./test.txt");
+        execute_create_file(path, "Hello world").expect("");
+        let path = Path::new("./test.txt");
         let expected_file_content: String = "Hello world".to_string();
         let actual_file_content: String =
             fs::read_to_string(path).expect("Cannot read file content");
         assert_eq!(expected_file_content, actual_file_content);
+    }
+
+    #[test]
+    fn should_delete_the_test_file() {
+        let path = Path::new("./test.txt");
+        fs::write(path, "").unwrap();
+        fs::remove_file(path).expect("Cannot remove file");
+        assert!(!fs::exists(path).expect("Cannot check if file is still there"));
     }
 }
