74aeb1ab691e6e60898aa2c1ce536fa1cfa0e3ae
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-14 15:01:36 +0300

    fix: remove unused and useless func

--- src/local_std/local_fs.rs
+++ src/local_std/local_fs.rs
@@ -1,33 +1,11 @@
-use std::{fs, path::Path};
-
-pub fn execute_create_dir(path: &Path) -> std::io::Result<()> {
-    fs::create_dir(path)?;
-    Ok(())
-}
-
-pub fn execute_delete_dir(path: &Path) -> std::io::Result<()> {
-    fs::remove_dir(path)?;
-    Ok(())
-}
-
-pub fn execute_create_file<T: AsRef<Path>, C: AsRef<[u8]>>(
-    path: T,
-    content: C,
-) -> std::io::Result<()> {
-    fs::write(path, content)?;
-    Ok(())
-}
-
 #[cfg(test)]
 mod test {
     use std::{fs, path::Path};
 
-    use crate::local_std::local_fs::{execute_create_dir, execute_create_file, execute_delete_dir};
-
     #[test]
     fn should_create_single_dir() {
         let path = Path::new("./first-dir-test");
-        execute_create_dir(path).expect("Cannot create test-output-folder");
+        fs::create_dir(path).expect("Cannot create test-output-folder");
         assert!(fs::exists(path).expect("Cannot check folder existance"));
         fs::remove_dir(path).expect("Cannot delete dir");
     }
@@ -43,7 +21,7 @@ mod test {
     #[test]
     fn should_create_test_file() {
         let path = Path::new("./test.txt");
-        execute_create_file(path, "Hello world").expect("");
+        fs::write(path, "Hello world").expect("");
         assert!(fs::exists(path).expect("Cannot check if file is there"));
         fs::remove_file(path).unwrap();
     }
@@ -51,7 +29,7 @@ mod test {
     #[test]
     fn should_read_file_content() {
         let path = Path::new("./test.txt");
-        execute_create_file(path, "Hello world").expect("");
+        fs::write(path, "Hello world").expect("");
         let path = Path::new("./test.txt");
         let expected_file_content: String = "Hello world".to_string();
         let actual_file_content: String =
